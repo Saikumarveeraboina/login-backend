4893cddc5ae42aa03366451dcb57ad48a5618da9
Saikumarveeraboina <saikumar96030433@gmail.com>
2025-11-26 23:09:53 +0530

    added last commit

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 dotenv.config();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+// allow preflight
+app.options("*", cors());
+
 app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
